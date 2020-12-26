b2049556d0ff06f453df8397c567f31cc2dd229b
hien040599 <60846482+hien040599@users.noreply.github.com>
2020-12-26 15:36:48 +0700

    cart page completed
    -bổ sung thêm 1 vài tính năng mới cho trang giỏ hàng

--- project/src/layout/ListCartItems/ListCartItems.js
+++ project/src/layout/ListCartItems/ListCartItems.js
@@ -14,45 +14,53 @@ function ListCartItems(props) {
 
   return (
     <div className="wrapper-list-cart">
-      <ul>
-        {item.map((value, index) => {
-          return (
-            <li key={index}>
-              <div className="wrapper-list-cart__img">
-                <img src={value.image} alt={value.name} />
-              </div>
-              <div className="wrapper-list-cart__infor">
-                <h4>
-                  <a href="google.com.vn">{value.name}</a>
-                </h4>
-                <p>Qty: {value.quantity}</p>
-                <p>${value.price}</p>
-                <div className="wrapper-list-cart__infor__Characteristics">
-                  <p>Color: {value.color}</p>
-                  <p>Size:{value.size}</p>
-                </div>
-              </div>
-              <div className="wrapper-list-cart__btn-delete">
-                <span>
-                  <i
-                    className="far fa-times-circle"
-                    onClick={() => deleteCartItem(value.id)}
-                  ></i>
-                </span>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
-      <div className="wrapper-list-cart__total-price">
-        <span>Total:</span>
-        <span>${totalCart.toFixed(1)}</span>
-      </div>
+      {item.length === 0 ? (
+        <p className="wrapper-list-cart__notification">
+          No items added to cart
+        </p>
+      ) : (
+        <div>
+          <ul>
+            {item.map((value, index) => {
+              return (
+                <li key={index}>
+                  <div className="wrapper-list-cart__img">
+                    <img src={value.image} alt={value.name} />
+                  </div>
+                  <div className="wrapper-list-cart__infor">
+                    <h4>
+                      <a href="google.com.vn">{value.name}</a>
+                    </h4>
+                    <p>Qty: {value.quantity}</p>
+                    <p>${value.price}</p>
+                    <div className="wrapper-list-cart__infor__Characteristics">
+                      <p>Color: {value.color}</p>
+                      <p>Size:{value.size}</p>
+                    </div>
+                  </div>
+                  <div className="wrapper-list-cart__btn-delete">
+                    <span>
+                      <i
+                        className="far fa-times-circle"
+                        onClick={() => deleteCartItem(value.id)}
+                      ></i>
+                    </span>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+          <div className="wrapper-list-cart__total-price">
+            <span>Total:</span>
+            <span>${totalCart.toFixed(1)}</span>
+          </div>
 
-      <div className="wrapper-list-cart__btn">
-        <MyLink lable={"VIEW CART"} to="/cart" activeExact={true} />
-        <MyLink lable={"CHECKOUT"} to="/checkout" activeExact={true} />
-      </div>
+          <div className="wrapper-list-cart__btn">
+            <MyLink lable={"VIEW CART"} to="/cart" activeExact={true} />
+            <MyLink lable={"CHECKOUT"} to="/checkout" activeExact={true} />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
